00ba3348fa92b2a7b525d340baa881021bad9ed6
Ehsanur Rahman <ehsanur.rahman@sazim.io>
2021-12-27 16:39:15 +0600

    Address the following changes -
    - Add jsconfig for relative path imports
    - Add styled-components and CDN code
    - Update styles
    - Create SignIn Page
    - Create custom form components
    - Add form resolver package so that RHF can handle form validation

---
+++ src/App.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledAppContainer = styled.div`
+  display: flex;
+  height: 100%;
+`
+
+export { StyledAppContainer };
---
+++ src/pages/signInPage/signInPage.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Container } from 'semantic-ui-react';
+
+const StyledSignInPageContainer = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+export { StyledSignInPageContainer };
