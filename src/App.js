4795e6e48f3d10314f6f7b33c747677ce6cf0fa9
Ehsanur Rahman <ehsanur.rahman@sazim.io>
2022-01-02 17:37:15 +0600

    Design Product card and user product list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { RequireAuth, PreSignIn } from 'shared/components';
+import { NOT_LOGGED_IN_STATUS } from 'slices/currentUser';
 
 import { SignInPage, RegistrationPage, UserProductListPage } from './pages';
 import { StyledAppContainer } from './App.styles'
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <>
-      <StyledAppContainer>
+      <StyledAppContainer isPreLogin={['/signin', '/registration', '/'].includes(pathname)}>
         <Routes>
           <Route
             path="/"
---
+++ src/pages/userProductListPage/userProductListPage.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledProductList = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const StyledProductCardHolder = styled.div`
+  margin-bottom: 10px;
+`
+
+export { StyledProductList, StyledProductCardHolder };
